e0b7ef920e140846702470fe29956f18e82737e2
Lautx <bakhrul.ulum001@gmail.com>
2022-01-21 21:56:15 +0700

    Create 6-WordBlanks.js

---
+++ Basic JavaScript/6-WordBlanks.js
@@ -0,0 +1,12 @@
+// Word Blanks
+// We will now use our knowledge of strings to build a "Mad Libs" style word game we're calling "Word Blanks". You will create an (optionally humorous) "Fill in the Blanks" style sentence.
+
+// In a "Mad Libs" game, you are provided sentences with some missing words, like nouns, verbs, adjectives and adverbs. 
+// You then fill in the missing pieces with words of your choice in a way that the completed sentence makes sense.
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+const wordBlanks = "My neighbour " + myNoun + " is small but has a " + myAdjective + " and muscular leg and could " + myVerb + " very " + myAdverb;
