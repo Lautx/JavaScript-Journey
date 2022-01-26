ff42878cd9756620d1dbecaea77a944b77ddddcc
Lautx <bakhrul.ulum001@gmail.com>
2022-01-26 10:41:17 +0700

    Create 7-Shopping-list.js

---
+++ Basic JavaScript/7-Shopping-list.js
@@ -0,0 +1,12 @@
+// Create a shopping list in the variable myList. The list should be a multi-dimensional array containing several sub-arrays.
+
+// The first element in each sub-array should contain a string with the name of the item. The second element should be a number representing the quantity i.e.
+
+["Chocolate Bar", 15]
+// There should be at least 5 sub-arrays in the list.
+
+const myList = [["Eggs",12],["Milk",2],["Water",2],["Rice",5],["Chili",50]];
+myList.shift()
+myList.unshift(["Oil",10])
+myList.pop()
+myList.push(["Gas",5])
