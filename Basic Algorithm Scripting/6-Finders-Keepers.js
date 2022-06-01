90e13092d85920bf5aee900087bf0e0cad05ab56
Lautx <bakhrul.ulum001@gmail.com>
2022-06-01 12:07:06 +0700

    Update 6-Finders-Keepers.js

--- Basic Algorithm Scripting/6-Finders-Keepers.js
+++ Basic Algorithm Scripting/6-Finders-Keepers.js
@@ -6,11 +6,9 @@ findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; }) //shou
 findElement([1, 3, 5, 9], function(num) { return num % 2 === 0; }) //should return undefined.
 
 function findElement(arr, func) {
-    let num = 0
-  
     for(let i = 0; i < arr.length; i++){
     let num = arr[i]
-    if (func(num)){
+    if (func(num) == true){
     return num
     } 
     } 
